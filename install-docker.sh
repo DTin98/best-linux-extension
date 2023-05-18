a540c7a7fabed2716ad2112291265e974872bc0b
Tin Truong <59561408+DTin98@users.noreply.github.com>
2023-05-18 14:10:13 +0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-
-# Updating existing list of packages
+# Update package list and upgrade all packages
 sudo apt update
+sudo apt upgrade -y
 
-# Installing prerequisite packages
+# Install prerequisites
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-# Adding the GPG key for the official Docker repository
+# Add Docker's official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# Adding the Docker repository to APT sources
+# Add Docker's stable repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
-# Updating the package database with the Docker packages
+# Update package list again
 sudo apt update
 
-# Installing Docker
+# Install Docker
 sudo apt install -y docker-ce
 
-# Checking Docker status
+# Verify Docker is installed and running
 sudo systemctl status docker
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to Docker Compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Verify Docker Compose is installed
+docker-compose --version
